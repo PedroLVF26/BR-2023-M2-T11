5a32ad74ca86c14d980b44d0e688f18f6f0c159b
PedroLVF26 <pedrolucasvf55@gmail.com>
2023-06-02 20:04:58 -0300

    Atualização Final

--- dino_runner/components/Obstacles/cactus.py
+++ dino_runner/components/Obstacles/cactus.py
@@ -1,17 +1,22 @@
-import random
+from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.obstacle import Obstacle
 
-from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
-from dino_runner.components.Obstacles.obstacle import Obstacle
 
-CACTUS = [
-   (LARGE_CACTUS, 300),
-   (SMALL_CACTUS, 325),
-]
 
 
 class Cactus(Obstacle):
     def __init__(self):
-        image, cactus_pos = CACTUS[random.randint(0, 1)]
-        self.type = random.randint(0, 2)
-        super().__init__(image, self.type)
-        self.rect.y = cactus_pos
+        super().__init__(SMALL_CACTUS, 0)
+        self.rect.y = 490
+        self.step_index = 0
+        self.image_index = 0
+
+    def draw(self, screen):
+        screen.blit(self.image[self.image_index], self.rect)
+        self.step_index += 1
+
+        if self.step_index >= 2:
+            self.step_index = 0
+            self.image_index += 1
+            if self.image_index >= len(self.image):
+                self.image_index = 0
--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,16 +1,15 @@
 import pygame
+import pygame.mixer
 from pygame.sprite import Sprite
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING, DEFAULT_TYPE, SHIELD_TYPE, HAMMER_TYPE, DUCKING_HAMMER, DUCKING_SHIELD, JUMPING_HAMMER, JUMPING_SHIELD, RUNNING_HAMMER, RUNNING_SHIELD, JUMP, SLOW, SLOW_TYPE
 
-from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING, DEFAULT_TYPE, SHIELD_TYPE, DUCKING_SHIELD, JUMPING_SHIELD, RUNNING_SHIELD
-
+DUCK_IMG = { DEFAULT_TYPE: DUCKING, SHIELD_TYPE: DUCKING_SHIELD, HAMMER_TYPE: DUCKING_HAMMER, SLOW_TYPE: DUCKING }
+JUMP_IMG = { DEFAULT_TYPE: JUMPING, SHIELD_TYPE: JUMPING_SHIELD, HAMMER_TYPE: JUMPING_HAMMER, SLOW_TYPE: JUMPING }
+RUN_IMG = { DEFAULT_TYPE: RUNNING, SHIELD_TYPE: RUNNING_SHIELD, HAMMER_TYPE: RUNNING_HAMMER, SLOW_TYPE: RUNNING }
 X_POS = 80
-Y_POS = 310
-Y_POS_DUCK = 340
+Y_POS = 455
+Y_POS_DUCK = 500
 JUMP_VEL = 8.5
-
-DUCK_IMG = { DEFAULT_TYPE: DUCKING, SHIELD_TYPE: DUCKING_SHIELD }
-JUMP_IMG = { DEFAULT_TYPE: JUMPING, SHIELD_TYPE: JUMPING_SHIELD }
-RUN_IMG = { DEFAULT_TYPE: RUNNING, SHIELD_TYPE: RUNNING_SHIELD }
 
 
 class Dinosaur(Sprite):
@@ -20,18 +19,21 @@ class Dinosaur(Sprite):
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
-        self.step_index = 0
-        self.jump_vel = JUMP_VEL
-        self.dino_jump = False
+        self.step_index =  0
         self.dino_run = True
         self.dino_duck = False
+        self.dino_jump = False
+        self.dino_jump_vel = JUMP_VEL
+
         self.setup_state()
 
     def setup_state(self):
         self.has_power_up = False
         self.shield = False
+        self.hammer = False  
+        self.slow = False
         self.show_text = False
-        self.power_up_time = 0
+        self.shield_time_up = 0
 
     def update(self, user_input):
         if self.dino_run:
@@ -41,24 +43,30 @@ class Dinosaur(Sprite):
         elif self.dino_duck:
             self.duck()
 
-        if user_input[pygame.K_UP] and not self.dino_jump:
+        if user_input[pygame.K_w] and not self.dino_jump:
+            self.dino_run = False
             self.dino_jump = True
-            self.dino_run = False
+            effect_jump = pygame.mixer.Sound(JUMP)
+            effect_jump.play()
             self.dino_duck = False
-        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+
+        elif user_input[pygame.K_LSHIFT] and not self.dino_jump:
             self.dino_run = False
-            self.dino_jump = False 
+            self.dino_jump = False
             self.dino_duck = True
+
         elif not self.dino_jump and not self.dino_duck:
             self.dino_run = True
             self.dino_jump = False
             self.dino_duck = False
 
+
         if self.step_index >= 9:
             self.step_index = 0
+    
 
     def run(self):
-        self.image = RUN_IMG[self.type][self.step_index // 5]
+        self.image = RUN_IMG[self.type][self.step_index // 2]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
@@ -67,23 +75,20 @@ class Dinosaur(Sprite):
     def jump(self):
         self.image = JUMP_IMG[self.type]
         if self.dino_jump:
-            self.dino_rect.y -= self.jump_vel * 4
-            self.jump_vel -= 0.8
-
-        if self.jump_vel < -JUMP_VEL:
-            self.dino_rect.y = Y_POS
+            self.dino_rect.y -= self.dino_jump_vel * 5
+            self.dino_jump_vel -= 0.8
+        if self.dino_jump_vel < -JUMP_VEL:
+            self.dino_rect_y = Y_POS
             self.dino_jump = False
-            self.jump_vel = JUMP_VEL
+            self.dino_jump_vel = JUMP_VEL
 
     def duck(self):
-        self.image = DUCK_IMG[self.type][self.step_index // 5]
+        self.image = DUCK_IMG[self.type][self.step_index // 2]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS_DUCK
-        self.step_index += 1
         self.dino_duck = False
-
+        self.step_index += 1
+            
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
-
-    
